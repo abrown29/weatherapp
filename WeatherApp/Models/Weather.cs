694c8d22ae2a21f1fe233795a218fb9ef247228e
Amanda Brown <amanda.brown@gmail.com>
2016-10-26 14:36:20 -0400

    initial commit

---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public Object getWeatherForcast()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=26dc5253ae4cdb2d6fd4fe17352d602f&units=imperial";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+    }
+}
